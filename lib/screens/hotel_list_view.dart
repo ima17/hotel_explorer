4ea6e2ebabc913015bea018272dd2ad7b76f59b5
imantha <ima.sudaraka@outlook.com>
2023-07-14 06:05:33 +0530

    Create hotel list view

---
+++ lib/screens/hotel_list_view.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import '../models/hotel_data.dart';
+import '../services/hotel_data_service.dart';
+import 'hotel_details_view.dart';
+
+class HotelListView extends StatefulWidget {
+  const HotelListView({Key? key}) : super(key: key);
+
+  @override
+  HotelListViewState createState() => HotelListViewState();
+}
+
+class HotelListViewState extends State<HotelListView> {
+  late Future<List<Hotel>> futureHotels;
+
+  @override
+  void initState() {
+    super.initState();
+    futureHotels = HotelService.fetchHotels();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hotel List'),
+      ),
+      body: FutureBuilder<List<Hotel>>(
+        future: futureHotels,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(snapshot.data![index].image['small']!),
+                      ),
+                      title: Text(
+                        snapshot.data![index].title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      subtitle: Text(
+                        snapshot.data![index].address,
+                        style: const TextStyle(
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                HotelDetailView(hotel: snapshot.data![index]),
+                          ),
+                        );
+                      },
+                    ),
+                    const Divider(),
+                  ],
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text("${snapshot.error}"));
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
